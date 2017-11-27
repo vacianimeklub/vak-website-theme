f5feab3c7c8b42fee78128391485497c808875b9
Zoltán Adamek <djscorchio@gmail.com>
2017-11-27 23:55:38 +0100

    Sets up logo

--- header.php
+++ header.php
@@ -22,13 +22,14 @@
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
-
 	<header class="site-header" role="banner">
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
 			<div class="title-bar-left">
 				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php echo get_template_directory_uri() . "/"?>dist/assets/images/vaci-anime-klub-logo-500-opt.png" alt="<?php bloginfo( 'name' ); ?>" />
+					</a>
 				</span>
 			</div>
 		</div>
@@ -36,7 +37,9 @@
 		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php echo get_template_directory_uri() . "/"?>dist/assets/images/vaci-anime-klub-logo-500-opt.png" alt="<?php bloginfo( 'name' ); ?>" />
+					</a>
 				</div>
 			</div>
 			<div class="top-bar-right">
