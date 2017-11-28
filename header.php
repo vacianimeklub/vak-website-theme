26e091a54c3791cb4fd2565ad191d1dfc6052e2c
Zoltán Adamek <djscorchio@gmail.com>
2017-11-28 23:04:19 +0100

    Prepares front page, adds earlier prototype mods

--- header.php
+++ header.php
@@ -22,33 +22,44 @@
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
-	<header class="site-header" role="banner">
-		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
-			<div class="title-bar-left">
-				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php echo get_template_directory_uri() . "/"?>dist/assets/images/vaci-anime-klub-logo-500-opt.png" alt="<?php bloginfo( 'name' ); ?>" />
-					</a>
-				</span>
+	<?php if ( is_front_page() ): ?>
+		<header class="frontpage-header" role="banner">
+			<div class="frontpage-header-contents">
+				<img src="<?php echo get_template_directory_uri() . "/"?>dist/assets/images/vaci-anime-klub-logo-500-opt.png" alt="<?php bloginfo( 'name' ); ?>" />
+				<h1 class="frontpage-logo-title">Váci Anime Klub</h1>
+				<h2 class="frontpage-logo-subtitle">Csatlakozzá, dik!</h2>
+				<a href="/csatlakoznal" class="frontpage-cta-button button large">Csatlakozz! :)</a>
 			</div>
-		</div>
-
-		<nav class="site-navigation top-bar" role="navigation">
-			<div class="top-bar-left">
-				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php echo get_template_directory_uri() . "/"?>dist/assets/images/vaci-anime-klub-logo-500-opt.png" alt="<?php bloginfo( 'name' ); ?>" />
-					</a>
+		</header>
+	<?php else: ?>
+		<header class="site-header" role="banner">
+			<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle() ?>>
+				<div class="title-bar-left">
+					<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+					<span class="site-mobile-title title-bar-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<img src="<?php echo get_template_directory_uri() . "/"?>dist/assets/images/vaci-anime-klub-logo-500-opt.png" alt="<?php bloginfo( 'name' ); ?>" />
+						</a>
+					</span>
 				</div>
 			</div>
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
 
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
-			</div>
-		</nav>
+			<nav class="site-navigation top-bar" role="navigation">
+				<div class="top-bar-left">
+					<div class="site-desktop-title top-bar-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<img src="<?php echo get_template_directory_uri() . "/"?>dist/assets/images/vaci-anime-klub-logo-500-opt.png" alt="<?php bloginfo( 'name' ); ?>" />
+						</a>
+					</div>
+				</div>
+				<div class="top-bar-right">
+					<?php foundationpress_top_bar_r(); ?>
 
-	</header>
+					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+						<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+					<?php endif; ?>
+				</div>
+			</nav>
+		</header>
+	<?php endif; ?>
+	
--- index.php
+++ index.php
@@ -17,34 +17,65 @@ get_header(); ?>
 
 <div class="main-container">
 	<div class="main-grid">
-		<main class="main-content">
-		<?php if ( have_posts() ) : ?>
+		<?php if ( is_front_page() ): ?>
+			<main class="main-content-full-width">
+				<?php // move this to template-parts ?>
+				<div class="frontpage-main-features">
+					<div class="frontpage-main-feature">
+						<div class="main-feature-content">
+							<h4 class="main-feature-title">Események</h4>
+							<div class="main-feature-text"></div>
+						</div>
+					</div>
+					<div class="frontpage-main-feature">
+						<div class="main-feature-content">
+							<h4 class="main-feature-title">Cikkek</h4>
+							<div class="main-feature-text"></div>
+						</div>
+					</div>
+					<div class="frontpage-main-feature">
+						<div class="main-feature-content">
+							<h4 class="main-feature-title">VAKstars</h4>
+							<div class="main-feature-text"></div>
+						</div>
+					</div>
+					<div class="frontpage-main-feature">
+						<div class="main-feature-content">
+							<h4 class="main-feature-title">Galléria</h4>
+							<div class="main-feature-text"></div>
+						</div>
+					</div>
+				</div>
+			</main>
+		<?php else: ?>
+			<main class="main-content">
+			<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-			<?php endwhile; ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+				<?php endwhile; ?>
 
-			<?php else : ?>
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+				<?php else : ?>
+					<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-			<?php endif; // End have_posts() check. ?>
+				<?php endif; // End have_posts() check. ?>
 
-			<?php /* Display navigation to next/previous pages when applicable */ ?>
-			<?php
-			if ( function_exists( 'foundationpress_pagination' ) ) :
-				foundationpress_pagination();
-			elseif ( is_paged() ) :
-			?>
-				<nav id="post-nav">
-					<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-					<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-				</nav>
-			<?php endif; ?>
+				<?php /* Display navigation to next/previous pages when applicable */ ?>
+				<?php
+				if ( function_exists( 'foundationpress_pagination' ) ) :
+					foundationpress_pagination();
+				elseif ( is_paged() ) :
+				?>
+					<nav id="post-nav">
+						<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+						<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+					</nav>
+				<?php endif; ?>
 
-		</main>
-		<?php get_sidebar(); ?>
-
+			</main>
+			<?php get_sidebar(); ?>
+		<?php endif; ?>
 	</div>
 </div>
 <?php get_footer();
