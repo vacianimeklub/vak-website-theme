64ea81e206bd013dba6f0e9dcf6afcc771d40e1c
Zoltán Adamek <djscorchio@gmail.com>
2017-12-11 23:56:33 +0100

    Fixes hotjar code

--- template-parts/hotjar.php
+++ template-parts/hotjar.php
@@ -1,10 +1,17 @@
+<?php 
+
+$hotjar_site_id = get_option( 'hotjar_site_id' );
+
+if ($hotjar_site_id) { ?>
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:<?php get_option( 'hotjar_site_id' ); ?>,hjsv:6};
+        h._hjSettings={hjid:<?php print $hotjar_site_id ?>,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
+<?php 
+}
